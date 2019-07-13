c3d608556586a57ee316fe729a06421700d58644
marco <marco.ferrati@gmail.com>
2019-07-13 23:18:33 +0200

    Database things V1

--- User_Prodotti.py
+++ User_Prodotti.py
@@ -1,1 +1,7 @@
-
+class User_Prodotti():
+    def __init__(self, id, user_id, product_id, date, qt):
+        self.id = id
+        self.user_id = user_id
+        self.product_id = product_id
+        self.date = date
+        self.qantity = qt
