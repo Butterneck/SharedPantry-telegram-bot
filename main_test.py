c27cf10ecde7c855d49d666ef376302403e73d2f
marco <marco.ferrati@gmail.com>
2019-07-15 19:50:32 +0200

    Updated main_test

--- main_test.py
+++ main_test.py
@@ -1,5 +1,6 @@
 from db_connection import *
 import os
+import datetime
 
 os.system("rm dispensa.db")
 
@@ -10,12 +11,15 @@ db_manager.addUser("Filippo", 2)
 db_manager.addProduct("Caramelle", 5.0, 10)
 db_manager.addProduct("Birra", 2.25, 20)
 
-print(db_manager.getAllProduct()[1].quantity)
-db_manager.addTransaction(1, int(db_manager.getAllProduct()[1].id), 2)
-print(db_manager.getAllProduct()[1].name)
-print(db_manager.getAllProduct()[1].quantity)
+db_manager.addTransaction(1, int(db_manager.getAllProduct()[1].id), 1)
+db_manager.addTransaction(1, int(db_manager.getAllProduct()[0].id), 2)
+db_manager.addTransaction(2, int(db_manager.getAllProduct()[1].id), 1)
+db_manager.addTransaction(1, int(db_manager.getAllProduct()[0].id), 3)
+db_manager.addTransaction(2, int(db_manager.getAllProduct()[1].id), 1)
+db_manager.addTransaction(1, int(db_manager.getAllProduct()[0].id), 2)
 
-print(db_manager.getAllChatIds())
+for el in db_manager.getAcquistiIn(1, datetime.date.today()-datetime.timedelta(3600), datetime.date.today()+datetime.timedelta(3600)):
+    print(el.date, el.quantity, el.user_id, el.user_id)
 
 
 #print(db_manager.getUserId("Filippo"))
