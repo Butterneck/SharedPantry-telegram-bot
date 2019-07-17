8defb5d9758a74cf40a1912c41fbd1617964e1e6
Butterneck <pinton.filippo@protonmail.com>
2019-07-17 22:49:07 +0200

    Modificato db_url

--- globalVariables.py
+++ globalVariables.py
@@ -5,7 +5,7 @@ global db_manager
 db_manager = DB_Connection(DB_URL)
 
 global db_manager_for_test
-db_manager_for_test = DB_Connection(DB_URL="postgres://eyicwwxwnxuxou:675f943b8955a12022eab26b184c193d3c1219c4999688d899cfb3687f33e1b4@ec2-54-217-234-157.eu-west-1.compute.amazonaws.com:5432/df8fia9ccanh45")
+db_manager_for_test = DB_Connection(DB_URL="postgres://kztdqvcvabikuw:558e67b9ac1b55d22d94a8790b5bde3918d69e89e20f136cf89e25fdbc8d39e4@ec2-54-220-0-91.eu-west-1.compute.amazonaws.com:5432/d1q4vs74un4gp1")
 
 global nuovoProdotto
 nuovoProdotto = None
