1b50f6e4e4e08982f432717c8cc102c5f1a326fb
Butterneck <pinton.filippo@protonmail.com>
2019-07-17 22:30:31 +0200

    Aggiunto init_db

--- db_connection_postgresql.py
+++ db_connection_postgresql.py
@@ -12,6 +12,8 @@ from Prodotti import *
 
 DB_URL = os.environ['DATABASE_URL']
 #DB_URL = "postgres://eyicwwxwnxuxou:675f943b8955a12022eab26b184c193d3c1219c4999688d899cfb3687f33e1b4@ec2-54-217-234-157.eu-west-1.compute.amazonaws.com:5432/df8fia9ccanh45"
+init_db = open("dispensa.sql").read()
+
 
 
 class DB_Connection():
