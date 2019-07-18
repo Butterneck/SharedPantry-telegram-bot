7eabb5eb898c5720795e4fe65695e482917020f8
Butterneck <pinton.filippo@protonmail.com>
2019-07-18 20:07:58 +0200

    Aggiunto thread per il controllo dell'activator

---
+++ MyThread.py
@@ -0,0 +1,15 @@
+from threading import Thread
+import globalVariables as gv
+from contabilita import debitoMensile
+
+class MyThread(Thread):
+    def __init__(self):
+        '''Constructor'''
+        Thread.__init__(self)
+
+    def run(self):
+        while True:
+            if gv.db_manager.checkActivator():  # Check the value of activator
+                debitoMensile()
+                gv.db_manager.deactivateActivator() # reset activator value
+                return
