40859896fba91348e411e98ee1bb85f47bf0a682
Butterneck <pinton.filippo@protonmail.com>
2019-07-22 15:32:23 +0200

    Modificato delay del thread

--- Activator.py
+++ Activator.py
@@ -3,11 +3,3 @@ import globalVariables as gv
 class Activator():
     def __init__(self, activator):
         self.activator = activator
-
-    def run(self):
-        while True:
-            if gv.db_manager.checkActivator():  # Check the value of activator
-                debitoMensile(self.bot)
-                gv.db_manager.deactivateActivator() # reset activator value
-            print('sono qui')
-            time.sleep(3)
