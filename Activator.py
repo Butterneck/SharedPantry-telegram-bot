37658c502945cec9b277f9c163cd1d816f06e67e
Filippo <pinton.filippo@protonmail.com>
2020-01-13 22:21:35 +0100

    Updated imports

--- Activator.py
+++ Activator.py
@@ -1,5 +1,3 @@
-import globalVariables as gv
-
 class Activator():
     def __init__(self, activator):
         self.activator = activator
--- Backup.py
+++ Backup.py
@@ -1,5 +1,3 @@
-import globalVariables as gv
-
 class Backup():
     def __init__(self, backup):
         self.backup = backup
