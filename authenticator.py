e5796d019ded5db028e9d3ec1490f3244a47bb9d
Filippo <pinton.filippo@protonmail.com>
2020-01-13 22:21:16 +0100

    Added authenticator to check if users are authenticated

---
+++ authenticator.py
@@ -0,0 +1,12 @@
+class Authenticator():
+    Ciano = 879140791  # Ciano
+    Filippo = 32345162  # Filippo
+    Marco = 179624122  # Marco
+    admin_ids = [Ciano, Filippo, Marco]
+
+    def checkAuthentication(self, update):
+        if update.message.chat_id not in self.admin_ids:
+            print("Utente " + str(update.message.chat_id) + " non registrato")
+            return False
+        else:
+            return True
