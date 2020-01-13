020a687fdcf166c6daf28e43336d5ab057e6daeb
Filippo <pinton.filippo@protonmail.com>
2020-01-13 22:53:23 +0100

    Removed unused references

--- globalVariables.py
+++ globalVariables.py
@@ -1,23 +1,2 @@
-from db_connection_postgresql import *
-import os
-from Utils import terminalColors
-from Utils import Mese
-
-
-
-global chat_id_list
-chat_id_list = db_manager.getAllChatIds()
-db_manager.cleanCursor()
-
 global nuovoProdotto
 nuovoProdotto = None
-
-global Ciano
-Ciano = 879140791 #Ciano
-global Filippo
-Filippo = 32345162 #Filippo
-global Marco
-Marco = 179624122 #Marco
-
-global admin_id
-admin_id = [Ciano, Filippo, Marco]
