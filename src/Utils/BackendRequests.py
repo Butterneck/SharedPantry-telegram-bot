532f09a2e989ad55575f4c8bcdce9e55d3d508da
Filippo <pinton.filippo@protonmail.com>
2020-01-23 16:43:43 +0100

    Added response validator

--- src/Utils/BackendRequests.py
+++ src/Utils/BackendRequests.py
@@ -1,10 +1,24 @@
 from requests import post
 from os import environ
 
+import logging
+
 
 def request(path, data = {}):
     json = {
         'token': environ['BACKEND_TOKEN'],
         'data': data
     }
+    logging.info('requesting ' + path)
     return post(environ['BACKEND_URL'] + path, json=json)
+
+
+def validate_response(r):
+    if r.status_code == 200:
+        return True
+    elif r.status_code == 500:
+        logging.error('request returned 500, stopping')
+        return False
+    elif r.status_code == 403:
+        logging.error('backend got wrong bot token')
+        return False
