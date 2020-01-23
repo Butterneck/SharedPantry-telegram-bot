745c6c69cab312a18754602858bfb49723fcac75
Filippo <pinton.filippo@protonmail.com>
2020-01-23 16:38:42 +0100

    Default data parameter

--- src/Utils/BackendRequests.py
+++ src/Utils/BackendRequests.py
@@ -2,7 +2,7 @@ from requests import post
 from os import environ
 
 
-def request(path, data):
+def request(path, data = {}):
     json = {
         'token': environ['BACKEND_TOKEN'],
         'data': data
