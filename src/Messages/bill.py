d08976b56b19dcce2b5e296743af08613dff55c1
Filippo <pinton.filippo@protonmail.com>
2020-01-24 01:11:16 +0100

    Fixed json encoding of dates

--- src/Messages/bill.py
+++ src/Messages/bill.py
@@ -15,8 +15,8 @@ def send_bill_message(bot, update):
 def get_bill_message(chat_id):
     r = request('/getAcquistiIn', {
         'user_id': chat_id,
-        'start_date': datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0),
-        'end_date': datetime.now().replace(microsecond=0)
+        'start_date': str(datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)),
+        'end_date': str(datetime.now().replace(microsecond=0))
     })
     if not validate_response(r):
         return
@@ -31,14 +31,14 @@ def get_bill_message(chat_id):
     singleTransaction = remove_duplicated_products_in_transactions(allTransactions)
 
     totalPrice = 0
-    message = "This month you bought this:"
+    message = "This month you bought this:\n"
 
     for transaction in singleTransaction:
-        quantity = get_frequency(transaction.product_id, allTransactions)
-        product = list(filter(lambda el: el.id == transaction.product_id, allProducts))[0]
-        partialPrice = int(product.price * 100) * quantity
+        quantity = get_frequency(transaction['product_id'], allTransactions)
+        product = list(filter(lambda el: el['id'] == transaction['product_id'], allProducts))[0]
+        partialPrice = int(product['price'] * 100) * quantity
         totalPrice += partialPrice
-        message = message + product.name + ' x' + str(quantity) + '= €' + str(partialPrice/100) + '\n'
+        message = message + product['name'] + ' x' + str(quantity) + '= €' + str(partialPrice/100) + '\n'
 
     if totalPrice:
         message = message + 'Total bill: €' + str(totalPrice/100)
@@ -51,7 +51,7 @@ def get_bill_message(chat_id):
 def remove_duplicated_products_in_transactions(transactions):
     retList = []
     for transaction in transactions:
-        if transaction.product_id not in list(map(lambda el: el.product_id, retList)):
+        if transaction['product_id'] not in list(map(lambda el: el['product_id'], retList)):
             retList.append(transaction)
     return retList
 
@@ -59,6 +59,6 @@ def remove_duplicated_products_in_transactions(transactions):
 def get_frequency(product_id, transactions):
     counter = 0
     for transaction in transactions:
-        if product_id == transaction.product_id:
+        if product_id == transaction['product_id']:
             counter += 1
     return counter
