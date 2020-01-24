cd06d2722c36512f18553d9f9340d56213d42476
Filippo <pinton.filippo@protonmail.com>
2020-01-24 01:12:22 +0100

    Added authentication functions

--- src/Auth/authenticator.py
+++ src/Auth/authenticator.py
@@ -1,12 +1,34 @@
-class Authenticator():
-    Ciano = 879140791  # Ciano
-    Filippo = 32345162  # Filippo
-    Marco = 179624122  # Marco
-    admin_ids = [Ciano, Filippo, Marco]
+import json
+import logging
 
-    def checkAuthentication(self, update):
-        if update.message.chat_id not in self.admin_ids:
-            print("Utente " + str(update.message.chat_id) + " non registrato")
+from src.Utils.BackendRequests import request
+
+
+class Authenticator():
+    def checkUserExistence(self, chat_id):
+        logging.info('Checking user ' + str(chat_id) + ' existence')
+        r = request('/getUserFromChatId', {'chat_id': chat_id})
+        if r.status_code == 200:
+            return json.loads(r.text)['user'] is not None
+        elif r.status_code == 500:
+            logging.info('User ' + chat_id + ' is not logged in')
             return False
-        else:
-            return True
+        elif r.status_code == 403:
+            logging.warning('Wrong backend token')
+            exit(1)
+
+    def checkUserAdmin(self, chat_id):
+        r = request('/getUserFromChatId', {'chat_id': chat_id})
+        if r.status_code == 200:
+            if json.loads(r.text)['is_admin']:
+                logging.info('User ' + json.loads(r.text)['username'] + ' is an admin')
+                return True
+            else:
+                logging.info('User ' + json.loads(r.text)['username'] + ' is not an admin')
+                return False
+        elif r.status_code == 500:
+            logging.info('User ' + str(chat_id) + ' is not logged in')
+            return False
+        elif r.status_code == 403:
+            logging.warning('Wrong backend token')
+            exit(1)
