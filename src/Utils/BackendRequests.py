74ff7740d9d496904e3ae8ce3a0b8be466c8ae19
Filippo <pinton.filippo@protonmail.com>
2020-02-01 00:10:03 +0100

    Updated api path

--- src/Utils/BackendRequests.py
+++ src/Utils/BackendRequests.py
@@ -7,7 +7,7 @@ import logging
 def request(path, data=None):
     logging.info('requesting ' + path)
     logging.info('token:' + environ['BACKEND_TOKEN'])
-    r = post(environ['BACKEND_URL'] + path, json=data, headers={'token': environ['BACKEND_TOKEN']})
+    r = post(environ['BACKEND_URL'] + '/api' + path, json=data, headers={'token': environ['BACKEND_TOKEN']})
     return r
 
 
