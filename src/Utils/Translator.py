dbbbadf533ba7edb099cc9d2774ed003a0bd121f
Filippo <pinton.filippo@protonmail.com>
2020-02-02 18:57:17 +0100

    Updated backend request for translations

--- src/Utils/Translator.py
+++ src/Utils/Translator.py
@@ -8,8 +8,8 @@ def load_translations():
 
 
 def translate(string, chat_id):
-    lang = request('/getUserLangFromChatId', {
+    lang = request('/getUserFromChatId', {
         'chat_id': chat_id
-    })
+    })['user']['lang']
     t = load_translations()
     return t[string][lang]
